80154a221e84daf93f2ba1ee2003dddcc995f50b
haobo724 <s724724@gmail.com>
2022-02-01 21:07:39 +0100

    bat

---
+++ run.bat
@@ -0,0 +1,9 @@
+start "" cmd /k "conda activate pachong && python read.py"
+
+REM  change pachong to your envs i.e fe 
+
+	REM parser.add_argument('--febfile_name', type=str, help='', default=r'breast.feb')
+    REM parser.add_argument('--dcm_name', type=str, help='', default='breast.dcm')
+    REM parser.add_argument('--Node_name', type=str, help='', default='breast')
+    REM parser.add_argument('--mat_name', type=str, help='', default='matbreast')
+	REM  you can set arguments i.e python read.py --dcm_name abc.dcm --mat_name def --Node_name sadad 
